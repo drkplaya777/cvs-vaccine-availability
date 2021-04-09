585b707a85938b85c7c151bb2b4a61419c202274
Jeremy Walker <walkej19@gmail.com>
2021-04-08 22:43:02 -0400

    Adding polling interval to run_add_new_watcher.py

--- run_add_new_watcher.py
+++ run_add_new_watcher.py
@@ -17,24 +17,27 @@ from app.celery_tasks import check_cvs_for_immunization_availability
               help="email address to send the CVS email from")
 @click.option("--subject", default=const.EMAIL_SUBJECT_CSV_AVAILABILITY,
               help="subject of the CVS email")
+@click.option("--polling_interval", default=300,
+              help="how often should I check for availability")
 def add_new_watcher(
         name: str, email: str,  cities: str, state: str,
-        from_address: str, subject: str
+        from_address: str, subject: str, polling_interval: int
 ):
     click.echo('Adding new watcher')
-    click.echo(f'Recipient name: {name}')
+    click.echo(f'Recipient name: {name.title()}')
     click.echo(f'Email Address: {email}')
     click.echo(f'State: {state.upper()}')
     click.echo(f'From Email Address: {from_address}')
-    click.echo(f'Email subject {subject}')
+    click.echo(f'Email subject: {subject}')
+    click.echo(f'Polling interval: {polling_interval}')
 
     cities = explode_cites_into_a_list(cities)
 
-    click.echo(f'Adding {name} to CVS queue')
+    click.echo(f'Adding {name.title()} to CVS queue')
 
     check_cvs_for_immunization_availability.apply_async(
-        args=[from_address, email, subject, cities, name,
-              state.upper(), subject],
+        args=[from_address, email, subject, cities, name.title(),
+              state.upper(), polling_interval],
         queue="cvs"
     )
 
